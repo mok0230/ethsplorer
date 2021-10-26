4219234b5c9f26d652dfa46f636d6b862c6cc9fb
mok <mok0230@protonmail.com>
2021-10-25 23:36:28 -0700

    change pattern for updating blocks to handle both init and push/unshift cases

--- src/utils.ts
+++ src/utils.ts
@@ -2,13 +2,31 @@ import dayjs from 'dayjs';
 
 export const initializeBlockData = async (state: any, dispatch: any) => {
   const blockCount = await updateBlockCount(state, dispatch);
+
+  // initialize first block
+  const initBlockData = addBlock(state, dispatch, blockCount, "pushBlock");
+
+  const minBlockNum = updateMinBlockNum(dispatch, blockCount);
   updateGasPrice(state, dispatch);
+  updateBlocks(state, dispatch, minBlockNum, blockCount, [initBlockData]);
 
-  for (let i = blockCount; i > blockCount - 10; i--) {
-    await addBlock(state, dispatch, i);
-  }
+  openWebSocketConnection(state, dispatch);
+}
 
-  dispatch("confirmDataInitialized");
+const openWebSocketConnection = async (state: any, dispatch: any) => {
+  const websocket = new WebSocket(process.env.REACT_APP_ETH_NODE_WSS_URL as string);
+
+  websocket.onopen = (evt) => {
+    websocket.send(JSON.stringify({"jsonrpc":"2.0", "id": 1, "method": "eth_subscribe", "params": ["newHeads"]}))
+  };
+
+  websocket.onmessage = (evt) => { 
+    console.log('message received');
+    const parsedMessage = JSON.parse(evt.data);
+    if (parsedMessage.params && parsedMessage.params.result) {
+      dispatch({ type: "updateBlockCount", value: parseInt(parsedMessage.params.result.number, 16) })
+    }
+  };
 }
 
 const updateBlockCount = async (state: any, dispatch: any): Promise<number> => {
@@ -23,7 +41,13 @@ const updateBlockCount = async (state: any, dispatch: any): Promise<number> => {
   return blockCount;
 }
 
-const updateGasPrice = async (state: any, dispatch: any): Promise<number> => {
+const updateMinBlockNum = (dispatch: any, currentBlockNum: number): number => {
+  const minBlockNum = currentBlockNum - 10;
+  dispatch({ type: "updateMinBlockNum", value: minBlockNum });
+  return minBlockNum;
+}
+
+export const updateGasPrice = async (state: any, dispatch: any): Promise<number> => {
   const gasPrice = await state.nodeProvider.getGasPrice();
   console.log('gasPrice', gasPrice);
 
@@ -35,16 +59,31 @@ const updateGasPrice = async (state: any, dispatch: any): Promise<number> => {
   return gasPrice;
 }
 
-const addBlock = async (state: any, dispatch: any, blockNumber: number): Promise<boolean> => {
+export const updateBlocks = async (state: any, dispatch: any, minBlockNum: number, blockCount: number, existingBlocks: any[]) => {
+  let minBlockNumFetched = existingBlocks[0].number;
+  let maxBlockNumFetched = existingBlocks[existingBlocks.length - 1].number;
+
+  while (minBlockNumFetched > minBlockNum) {
+    minBlockNumFetched -= 1;
+    await addBlock(state, dispatch, minBlockNumFetched, 'unshiftBlock')
+  }
+
+  while (maxBlockNumFetched < blockCount) {
+    maxBlockNumFetched += 1;
+    await addBlock(state, dispatch, maxBlockNumFetched, 'pushBlock')
+  }
+}
+
+const addBlock = async (state: any, dispatch: any, blockNumber: number, dispatchType: 'pushBlock' | 'unshiftBlock'): Promise<any> => {
   const blockData = await state.nodeProvider.getBlock(blockNumber);
   console.log('blockData', blockData);
 
   dispatch({
-    type: 'addBlock',
+    type: dispatchType,
     value: blockData
   });
 
-  return true;
+  return blockData;
 }
 
 export const generateBlockTableData = (state: any) => {
@@ -58,7 +97,7 @@ export const generateBlockTableData = (state: any) => {
       gasLimit: block.gasLimit.toNumber().toLocaleString("en-US"),
       gasUsed: block.gasUsed.toNumber().toLocaleString("en-US")
     }
-  })
+  }).reverse();
 }
 
 export const generateChartData = (state: any) => {
@@ -67,79 +106,7 @@ export const generateChartData = (state: any) => {
   return state.blocks.map((block: any) => {
     return {
       timestamp: dayjs.unix(block.timestamp).format('h:mm:ss'),
-      // timestamp: block.timestamp.toString().slice(block.timestamp.toString().length - 1),
       gasUsed: block.gasUsed.toNumber() / (10 ** 6)
     }
   })
 }
-
-// Available methods:
-
-// https://docs.ethers.io/ethers.js/v3.0/html/api-providers.html
-
-// ** ACCOUNT **
-
-// prototype . getBalance ( addressOrName [ , blockTag ] )
-// Returns a Promise with the balance (as a BigNumber) of addressOrName at blockTag. (See: Block Tags)
-
-// default: blockTag=”latest”
-
-// prototype . getTransactionCount ( addressOrName [ , blockTag ] )
-// Returns a Promise with the number of sent transactions (as a Number) from addressOrName at blockTag. This is also the nonce required to send a new transaction. (See: Block Tags)
-
-// default: blockTag=”latest”
-
-// prototype . lookupAddress ( address )
-// Returns a Promise which resolves to the ENS name (or null) that address resolves to.
-// prototype . resolveName ( ensName )
-
-// ** Blockchain Status **
-
-// Returns a Promise with the block at blockHashorBlockNumber. (See: Block Responses)
-// prototype . getTransaction ( transactionHash )
-// Returns a Promise with the transaction with transactionHash. (See: Transaction Responses)
-// prototype . getTransactionReceipt ( transactionHash )
-// Returns a Promise with the transaction receipt with transactionHash. (See: Transaction Receipts)
-
-// ** Events **
-
-// These methods allow management of callbacks on certain events on the blockchain and contracts. They are largely based on the EventEmitter API.
-
-// prototype . on ( eventType , callback )
-// Register a callback for any future eventType; see below for callback parameters
-// prototype . once ( eventType , callback)
-// Register a callback for the next (and only next) eventType; see below for callback parameters
-// prototype . removeListener ( eventType , callback )
-// Unregister the callback for eventType; if the same callback is registered more than once, only the first registered instance is removed
-// prototype . removeAllListeners ( eventType )
-// Unregister all callbacks for eventType
-// prototype . listenerCount ( [ eventType ] )
-// Return the number of callbacks registered for eventType, or if ommitted, the total number of callbacks registered
-// prototype . resetEventsBlock ( blockNumber )
-// Begin scanning for events from blockNumber. By default, events begin at the block number that the provider began polling at.
-// Event Types
-
-// “block”
-// Whenever a new block is mined
-
-// callback( blockNumber )
-
-// any address
-// When the balance of the corresponding address changes
-
-// callback( balance )
-
-// any transaction hash
-// When the corresponding transaction is mined; also see Waiting for Transactions
-
-// callback( transaction )
-
-// an array of topics
-// When any of the topics are triggered in a block’s logs; when using the Contract API, this is automatically handled;
-
-// callback( log )
-
-// Waiting for Transactions
-
-// prototype . waitForTransaction ( transactionHash [ , timeout ] )
-// Return a Promise which returns the transaction once transactionHash is mined, with an optional timeout (in milliseconds)
