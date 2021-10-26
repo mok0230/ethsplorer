71d3432cb047d68dceb4ec79fccdcff6e73afb56
mok <mok0230@protonmail.com>
2021-10-25 23:33:25 -0700

    add reducer cases for min block and push/unshift blocks

--- src/AppProvider.jsx
+++ src/AppProvider.jsx
@@ -5,10 +5,16 @@ const reducer = (state, action) => {
   console.log('hitting this reducer?')
   switch (action.type) {
     case "updateBlockCount":
-      console.log('updateBlockCount')
+      console.log('updateBlockCount');
       return {
         ...state,
         blockCount: action.value
+      }
+    case "updateMinBlockNum":
+      console.log('updateBlockCount');
+      return {
+        ...state,
+        minBlockNum: action.value
       }
     case "updateGasPrice":
       console.log('updateGasPrice')
@@ -16,14 +22,32 @@ const reducer = (state, action) => {
         ...state,
         gasPrice: action.value
       }
-    case "addBlock":
-      console.log('addBlock')
+    case "pushBlock":
+      console.log('pushBlock')
       console.log('what is state?', state)
       return {
         ...state,
         blocks: [...state.blocks, action.value]
       }
+    case "unshiftBlock":
+      console.log('unshiftBlock')
+      console.log('what is state?', state)
+      return {
+        ...state,
+        blocks: [action.value, ...state.blocks]
+      }
+    case "cacheNewlyMinedBlockNumber":
+      return {
+        ...state,
+        newlyMinedBlockNumbers: [...state.newlyMinedBlockNumbers, action.value]
+      }
+    case "removeFirstItemFromNewlyMinedBlockCache":
+      return {
+        ...state,
+        newlyMinedBlockNumbers: [...state.newlyMinedBlockNumbers.slice(1)]
+      }
     case "confirmDataInitialized":
+      console.log('zzz confirming data is now initialized')
       return {
         ...state,
         isDataInitialized: true
@@ -35,10 +59,13 @@ const reducer = (state, action) => {
 
 const initialState = {
   nodeProvider: new ethers.providers.JsonRpcProvider(process.env.REACT_APP_ETH_NODE_HTTP_URL),
+  // nodeWebSocket: new WebSocket(process.env.REACT_APP_ETH_NODE_WSS_URL),
   blockCount: null,
+  minBlockNum: null,
   gasPrice: null,
   blocks: [],
-  isDataInitialized: false
+  isDataInitialized: false,
+  newlyMinedBlockNumbers: []
 }
 
 export const AppContext = React.createContext({
