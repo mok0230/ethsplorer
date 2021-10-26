a595f772a890ebe4a5ba14f53a600059bc5c0c4a
mok <mok0230@protonmail.com>
2021-10-25 23:34:12 -0700

    manage updating of current block/gas with useEffect

--- src/CurrentBlock.tsx
+++ src/CurrentBlock.tsx
@@ -2,18 +2,24 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
 import { AppContext } from "./AppProvider"
+import { updateGasPrice, updateBlocks } from "./utils"
 
 export default function CurrentBlock() {
-  const { state } = React.useContext(AppContext) as any;
-
-  console.log('when is this rendered?');
-  console.log('state', state)
+  const { state, dispatch } = React.useContext(AppContext) as any;
+  React.useEffect(() => {
+    updateGasPrice(state, dispatch);
+    if (state.blocks && state.blocks.length) {
+      updateBlocks(state, dispatch, state.minBlockNum, state.blockCount, state.blocks);
+    }
+    
+    // TODO: update block source data if necessary
+  }, [state.blockCount, state.minBlockNum]);
 
   return (
     <React.Fragment>
       <Title>Current Block Number</Title>
       <Typography component="p" variant="h4">
-        {state.blockCount}
+        <span id="current-block-number">{state.blockCount}</span>
       </Typography>
       {state.gasPrice && <Typography color="text.secondary" sx={{ flex: 1 }}>
         Current gas price: {state.gasPrice.toNumber().toLocaleString('en-US')}
